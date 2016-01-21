7e2eecae60754058ebd1029c4cf8397e6b570b4f
Satu Häkkinen <s.j.hakkinen@gmail.com>
2016-01-21 21:39:09 +0200

    Kaikki arvokkaassa järjestyksessä

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,10 @@
-# kirjoita koodi tänne
+def arvojarjestys(hashi)
+  hashi.values.reject{|item| item == 0}.sort
+end
+
+hash = {}
+hash["eka"] = 0
+hash["toka"] = 5
+hash["kolmas"] = 1
+hash["neljas"] = 10
+arvojarjestys(hash)
